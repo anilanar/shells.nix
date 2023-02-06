be553958b9febaec3a3288996c46b8438bb3f3d3
Anil Anar <anilanar@hotmail.com>
2023-02-07 00:47:48 +0100

    init

--- example/flake.nix
+++ example/flake.nix
@@ -2,15 +2,15 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     flake-parts.url = "github:hercules-ci/flake-parts";
-    shells.url = "github:anilanar/shells.nix";
     vscode-extensions.url = "github:nix-community/nix-vscode-extensions";
+    shells.url = "github:anilanar/shells.nix";
   };
   outputs = inputs@{ self, nixpkgs, flake-parts, shells, ... }:
     flake-parts.lib.mkFlake { inherit inputs; } {
       systems = nixpkgs.lib.systems.flakeExposed;
       imports = [ shells.flakeModule ];
       perSystem = { pkgs, ... }: {
-        config = {
+        config = mkShell {
           shells.packages = [ ];
           shells.env = { FOO = "BAR"; };
           js.enable = false;
