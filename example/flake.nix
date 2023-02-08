9f336ec3f92d9e6c31ff55afbf289ec192ccb834
Anil Anar <anilanar@hotmail.com>
2023-02-08 10:57:16 +0100

    fix example

--- example/flake.nix
+++ example/flake.nix
@@ -1,26 +1,21 @@
 {
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
-    flake-utils-plus.url = "github:gytis-ivaskevicius/flake-utils-plus";
-    vscode-extensions.url = "github:nix-community/nix-vscode-extensions";
+    flake-utils.url = "github:numtide/flake-utils";
     shells.url = "github:anilanar/shells.nix";
   };
-  outputs = inputs@{ self, nixpkgs, flake-utils-plus, shells, ... }:
-    flake-utils-plus.lib.mkFlake {
-      inherit self inputs;
-      outputsBuilder = channels:
-        let
-          pkgs = channels.nixpkgs;
-          unstable = channels.unstable;
-        in {
-          devShell = shells.lib.mkShell { inherit inputs; } {
-            shells.packages = [ ];
-            shells.env = { FOO = "BAR"; };
-            js.enable = false;
-            rust.enable = false;
-            cypress.enable = false;
-            turborepo.enable = false;
-          };
-        };
-    };
+  outputs = inputs@{ self, nixpkgs, flake-utils, shells, ... }:
+    flake-utils.lib.eachDefaultSystem (system: {
+      devShell = shells.lib.${system}.mkShell {
+        modules = [{
+          packages = [ ];
+          env = { FOO = "BAR"; };
+          vscode.enable = false;
+          js.enable = false;
+          rust.enable = false;
+          cypress.enable = false;
+          turborepo.enable = false;
+        }];
+      };
+    });
 }
