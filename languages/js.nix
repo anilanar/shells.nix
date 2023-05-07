3ccc255fd20f021f41c608256dc073f21ad718c0
Anil Anar <anilanar@hotmail.com>
2023-05-07 22:54:39 +0200

    allow overriding nodejs version

--- languages/js.nix
+++ languages/js.nix
@@ -1,9 +1,19 @@
 { config, types, lib, pkgs, ... }: {
-  options = { js = { enable = lib.mkEnableOption "js shell"; }; };
+  options = {
+    js = {
+      enable = lib.mkEnableOption "js shell";
+      nodejs = lib.mkOption {
+        type = types.package;
+        default = pkgs.nodejs-18_x;
+        defaultText = lib.literalExpression "pkgs.nodejs-18_x";
+        description = lib.mdDoc "Nodejs package to use.";
+      };
+    };
+  };
 
   config = {
     packages = lib.mkIf config.js.enable (with pkgs; [
-      nodejs-18_x
+      config.js.nodejs
       nodePackages.pnpm
       nodePackages.yarn
       automake
