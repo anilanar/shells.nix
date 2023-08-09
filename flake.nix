c5fe4362d230474ec919dc88cb8b521901403166
Anil Anar <anilanar@hotmail.com>
2023-08-09 17:52:29 +0200

    pin vscode version to before its gpu accel broke

--- flake.nix
+++ flake.nix
@@ -4,6 +4,8 @@
     fenix = { url = "github:nix-community/fenix"; };
     flake-utils.url = "github:numtide/flake-utils";
     vscode-extensions.url = "github:nix-community/nix-vscode-extensions";
+    vscode-gpu-workaround.url =
+      "github:nixos/nixpkgs/e8d56b900b2fb1d2237b5511d8a6e1d544943744";
   };
   description = "A library for creating personal devenvs";
   outputs = inputs@{ nixpkgs, flake-utils, vscode-extensions, ... }:
@@ -12,6 +14,14 @@
         pkgs = import inputs.nixpkgs {
           inherit system;
           config = { allowUnfree = true; };
+          overlays = [
+            (final: prev: {
+              vscode = (import inputs.vscode-gpu-workaround {
+                inherit system;
+                config = { allowUnfree = true; };
+              }).vscode;
+            })
+          ];
         };
         fenix = {
           inherit (inputs.fenix.packages.${system}.complete)
